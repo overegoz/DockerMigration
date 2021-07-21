fdd21c94228491c7d86b8b67169b8494996e1aa1
overegoz <twoon.kim@gmail.com>
2021-07-21 12:48:44 +0900

    sample udp program

--- sample-code/python-UDP/udp-ask-reply/client.py
+++ sample-code/python-UDP/udp-ask-reply/client.py
@@ -14,8 +14,6 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # 주소와 IP로 Bind
 sock.bind((localIP, localPort)) 
 sock.setblocking(0)  # non-blocking socket으로 만들기
 
-msg = "MSG from host A"
-
 def handler(signum, frame):
 	print("SIGINT handler called\n")
 	sock.close()
@@ -29,16 +27,16 @@ while(True):
 	# 상대방에게 데이터 전송하기
 	# bytes 객체로 변환해서 보내야 함
 	msg = "INCR " + str(counter)
-	counter = counter + 1
+	
 	sock.sendto(msg.encode(), (serverIP, serverPort))
-	print("sent: ", msg)
+	print("Sent : ", msg)
 	
 	time.sleep(0.5)
 	
 	try:
 		bytes, addr = sock.recvfrom(bufferSize) 
 		# 수신 데이터 출력하기	
-		print("Recv msg: ", bytes.decode())  # decode 해서 string객체로 변환
+		print("Recv : ", bytes.decode())  # decode 해서 string객체로 변환
 		#print("FROM: ", addr)  # (string ip, int port)
 		"""
 		print("type(bytes): ", type(bytes))  # bytes
@@ -47,9 +45,13 @@ while(True):
 		print("type(addr[0]): ", type(addr[0]))  # string
 		print("type(addr[1]): ", type(addr[1]))  # int
 		"""
+		
+		# 서버로 부터 응답을 받았을 경우에만 숫자를 변경하고
+		# 서버가 보내준 숫자를 그대로 사용함
+		counter = int(bytes.decode())
 	except socket.error:
 		# non-blocking recv: 빈손으로 리턴할때 예외가 발생하고, 이를 잡아줘야함
-		print("recvfrom: nothing")
+		print("Recv : nothing")
 		bytes = ""
 		addr = ""
 	
--- sample-code/python-UDP/udp-ask-reply/server.py
+++ sample-code/python-UDP/udp-ask-reply/server.py
@@ -2,7 +2,13 @@ import socket
 import time
 import signal, os
 
-localIP = "127.0.0.1"
+"""
+- 컨테이너는 기본적으로 0.0.0.0 IP 주소를 가진다. 따라서 서버 IP 주소를
+  0.0.0.0 으로 설정해 줘야 한다.
+- 포트 포워딩은 서버 수신 포트인 20002만 해 주면 된다.
+"""
+#localIP = "127.0.0.1"
+localIP = "0.0.0.0"
 localPort = 20002
 
 bufferSize = 1024
@@ -30,13 +36,13 @@ while(True):
 		#print("FROM: ", addr)
 		remoteIP, remotePort = addr[0], addr[1]
 		num = int(bytes.decode().split(' ')[1])
-		print("Recv: ", num)
+		#print("Recv: ", num)
 		msg = str(num+1)
 		
 		time.sleep(0.5)
 		# 상대방에게 데이터 전송하기
 		sock.sendto(msg.encode(), (remoteIP, remotePort))
-		print("sent: ", msg)
+		#print("sent: ", msg)
 	except socket.error:
 		# 클라이언트로 부터 요청/질문을 받지 않으면 답변할 게 없음
 		# non-blocking recv: 빈손으로 리턴할때 예외가 발생하고, 이를 잡아줘야함
