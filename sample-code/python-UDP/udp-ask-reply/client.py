2fd5d2d8c6267f7104b2a2146016f1553fa3dc50
overegoz <twoon.kim@gmail.com>
2021-07-21 13:34:28 +0900

    sample

--- sample-code/python-UDP/udp-ask-reply/client.py
+++ sample-code/python-UDP/udp-ask-reply/client.py
@@ -1,6 +1,7 @@
 import socket 
 import time
 import signal, os
+import datetime
 
 localIP = "127.0.0.1"
 localPort = 20001
@@ -21,22 +22,23 @@ def handler(signum, frame):
 
 signal.signal(signal.SIGINT, handler)
 counter = 0
+iter = 0
 print("Client begins!")
 while(True): 
 
 	# 상대방에게 데이터 전송하기
 	# bytes 객체로 변환해서 보내야 함
-	msg = "INCR " + str(counter)
-	
+	msg = "INCR " + str(counter) + " /" + str(iter)
 	sock.sendto(msg.encode(), (serverIP, serverPort))
-	print("Sent : ", msg)
+	iter = iter + 1
+	print(datetime.datetime.now(), " Sent : ", msg)
 	
 	time.sleep(0.5)
 	
 	try:
 		bytes, addr = sock.recvfrom(bufferSize) 
 		# 수신 데이터 출력하기	
-		print("Recv : ", bytes.decode())  # decode 해서 string객체로 변환
+		print(datetime.datetime.now(), " Recv : ", bytes.decode())  # decode 해서 string객체로 변환
 		#print("FROM: ", addr)  # (string ip, int port)
 		"""
 		print("type(bytes): ", type(bytes))  # bytes
@@ -45,14 +47,20 @@ while(True):
 		print("type(addr[0]): ", type(addr[0]))  # string
 		print("type(addr[1]): ", type(addr[1]))  # int
 		"""
-		
-		# 서버로 부터 응답을 받았을 경우에만 숫자를 변경하고
-		# 서버가 보내준 숫자를 그대로 사용함
-		counter = int(bytes.decode())
 	except socket.error:
 		# non-blocking recv: 빈손으로 리턴할때 예외가 발생하고, 이를 잡아줘야함
-		print("Recv : nothing")
+		print(datetime.datetime.now(), " Recv : nothing")
 		bytes = ""
 		addr = ""
-	
+		
+	"""
+	서버로 전달된 메시지는 큐잉 되어 있다가 결국은 전달된다.
+	따라서, 서버가 응답할때만 counter를 증가하게 되면 동일한 숫자를 반복적으로
+	서버에 전달하게 되어서 순서번호가 꼬이는 경우가 발생한다.
+	그냥 서버가 답하든 말든 REQUEST를 보내는 방식으로 하자. 
+	결국은 정상적으로 시간에 맞게 동작하게 된다.
+	그리고, 이렇게 해야, REQEST에 대한 RESPONSE가 언제 도착했는지
+	응답시간을 측정하기에도 좋다.
+	"""	
+	counter = counter + 1
 	time.sleep(0.5)
--- sample-code/python-UDP/udp-ask-reply/server.py
+++ sample-code/python-UDP/udp-ask-reply/server.py
@@ -1,7 +1,7 @@
 import socket 
 import time
 import signal, os
-
+import datetime
 """
 - 컨테이너는 기본적으로 0.0.0.0 IP 주소를 가진다. 따라서 서버 IP 주소를
   0.0.0.0 으로 설정해 줘야 한다.
@@ -25,6 +25,7 @@ def handler(signum, frame):
 
 signal.signal(signal.SIGINT, handler)
 print("Server start!")
+counter = 0
 while(True): 
 	time.sleep(0.5)
 	
@@ -32,12 +33,12 @@ while(True):
 	try:
 		bytes, addr = sock.recvfrom(bufferSize) 
 		# 수신 데이터 출력하기	
-		#print("MSG: ", bytes.decode())
+		print(datetime.datetime.now(), " Recv : ", bytes.decode())
 		#print("FROM: ", addr)
 		remoteIP, remotePort = addr[0], addr[1]
 		num = int(bytes.decode().split(' ')[1])
 		#print("Recv: ", num)
-		msg = str(num+1)
+		msg = str(num+1) + " " + bytes.decode().split(' ')[2]
 		
 		time.sleep(0.5)
 		# 상대방에게 데이터 전송하기
@@ -46,7 +47,7 @@ while(True):
 	except socket.error:
 		# 클라이언트로 부터 요청/질문을 받지 않으면 답변할 게 없음
 		# non-blocking recv: 빈손으로 리턴할때 예외가 발생하고, 이를 잡아줘야함
-		print("recvfrom: nothing")
+		print(datetime.datetime.now(), " Recv : nothing")
 		bytes = ""
 		addr = ""	
 	
