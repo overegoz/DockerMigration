9b0f030c39ce56c994d7c51388b97d63ca17e38c
overegoz <twoon.kim@gmail.com>
2021-07-29 14:18:18 +0900

    prof-1 rev rev

--- src/common.py
+++ src/common.py
@@ -34,8 +34,8 @@ ip = {controller_name : "127.0.0.1",
 ip = {controller_name : "192.168.0.2",
 		logger_name : "192.168.0.2",
 		user_name : "192.168.0.2",
-		ap1_name : "192.168.0.2",
-		ap2_name : "192.168.0.2",
+		ap1_name : "192.168.0.116",
+		ap2_name : "192.168.0.116",
 		edge_server1_name : "192.168.0.116",
 		edge_server2_name : "192.168.0.117"}
 
