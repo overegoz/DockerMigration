f1f6d5dc7b523b6fb7172643a3cda609392fc722
overegoz <twoon.kim@gmail.com>
2021-08-02 11:53:33 +0900

    ff

--- src/sudo-clear-files.sh
+++ src/sudo-clear-files.sh
@@ -5,3 +5,4 @@ rm -rf /home/daniel/migration/DiffCopyFiles/*
 rm -rf /home/daniel/migration/FC-CheckPoints/*
 rm -rf /home/daniel/migration/FullCopyImages/*
 rm -rf /home/daniel/migration/LogReplayRecords/*
+rm diff-*.txt
