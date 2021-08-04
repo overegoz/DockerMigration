a036e5474a38cb11626fca5eec92890d69e6e3bb
overegoz <twoon.kim@gmail.com>
2021-08-04 11:30:45 +0900

    aasdfsd

--- src/sudo-clear-files.sh
+++ src/sudo-clear-files.sh
@@ -2,15 +2,15 @@
 
 # ------------------------------------------------------------------
 # Full-Copy (FC) : 파일 지우지 말자... 나중에 필요할 듯
-#rm -rf /home/daniel/migration/FullCopyImages/*
-#rm -rf /home/daniel/migration/FC-CheckPoints/*
+rm -rf /home/daniel/migration/FullCopyImages/*
+rm -rf /home/daniel/migration/FC-CheckPoints/*
 # ------------------------------------------------------------------
 # Diff-Copy (DC) : 파일 지우지 말자... 나중에 필요할 듯
-#rm -rf /home/daniel/migration/DiffCopyFiles/*
-#rm -rf /home/daniel/migration/DC-CheckPoints/*
+rm -rf /home/daniel/migration/DiffCopyFiles/*
+rm -rf /home/daniel/migration/DC-CheckPoints/*
 # ------------------------------------------------------------------
 # Log-Replay (LR) : 파일 지우지 말자... 나중에 필요할 듯
-#rm -rf /home/daniel/migration/LogReplayRecords/*
+rm -rf /home/daniel/migration/LogReplayRecords/*
 # log-replay는 체크포인트 생성 안함: 체크포인트 저장을 위한 디렉토리가 없음
 # ------------------------------------------------------------------
 # 그 외...
