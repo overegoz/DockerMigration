cf60a22e3e3cb805da0efe3b5bc82ea6f3496373
overegoz <twoon.kim@gmail.com>
2021-08-24 11:27:33 +0900

    aa

--- src/common.py
+++ src/common.py
@@ -267,13 +267,13 @@ def action_profile(sock, es_name, profile):
 		send_log(sock, es_name, es_name, str2("ReplayTime", str(time_taken_sec)))
 	elif profile == 111:
 		sz = prof.get_diff_bit(profile) / (1000.0 * 8.0)
-		cmd = 'truncate -s {}M /tmp/file.file'.format(sz)
+		cmd = 'truncate -s {}M /tmp/file.file'.format( int(sz) )
 		print('action : ', cmd)
 		os.system(cmd)
 		time.sleep(prof.get_replay_sec(profile))
 	elif profile == 112:
 		sz = prof.get_diff_bit(profile) / (1000.0 * 8.0)
-		cmd = 'truncate -s {}M /tmp/file.file'.format(int(sz))
+		cmd = 'truncate -s {}M /tmp/file.file'.format( int(sz) )
 		print('action : ', cmd)
 		os.system(cmd)
 		time.sleep(prof.get_replay_sec(profile))
